a7e960030f0bf1aeb14f93dcdba191d9d0611ed1
AllenDev1 <dev.allen252@gmail.com>
2022-12-27 11:59:54 +0545

    :sparkles: using db from febval

--- api/datebase/sequelize.js
+++ api/datebase/sequelize.js
@@ -1,9 +1,24 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize({
-	dialect: "sqlite",
-	storage: "database.sqlite",
-});
+let sequelize;
+
+if (process.env.NODE_ENV === "production") {
+	sequelize = new Sequelize(
+		process.env.DB_NAME,
+		process.env.DB_USER,
+		process.env.DB_PASSWORD,
+		{
+			host: process.env.DB_HOST,
+			port: 3306,
+			dialect: "mysql",
+		}
+	);
+} else {
+	sequelize = new Sequelize({
+		dialect: "sqlite",
+		storage: "database.sqlite",
+	});
+}
 
 const connect = async () => {
 	try {
